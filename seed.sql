e100415ff5ce03f91653c607d4e1b9e27ce25b0b
Reddyvenumadhava <yellampallevenumadhavareddy@gmail.com>
2026-03-24 12:11:19 +0530

    Create seed.sql

---
+++ seed.sql
@@ -0,0 +1,11 @@
+-- Sample Data
+
+INSERT INTO tbl_cd_msalutation (value) VALUES
+('Mr'),
+('Ms'),
+('Mrs');
+
+INSERT INTO tbl_cd_mlanguages (language_code, language_name)
+VALUES
+('EN', 'English'),
+('HI', 'Hindi');
