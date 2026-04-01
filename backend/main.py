293a3a2b4ba8c7202e9accf555a9800b4c2bbf1d
Reddyvenumadhava <yellampallevenumadhavareddy@gmail.com>
2026-04-01 11:29:08 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "API working"}
+
+@app.get("/users")
+def get_users():
+    return {"users": ["Venu", "Ravi", "Kiran"]}
+
+@app.post("/add-user")
+def add_user(name: str):
+    return {"message": f"{name} added successfully"}
