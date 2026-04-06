eb6bc421df103ca161c7dbd40da500b871bfa838
Reddyvenumadhava <yellampallevenumadhavareddy@gmail.com>
2026-04-06 11:50:53 +0530

    Add user management endpoints for add, delete, update

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,27 @@ users = ["Venu", "Ravi", "Kiran"]
 def get_users():
     return {"users": users}
 
+
 @app.post("/add-user")
 def add_user(name: str):
     users.append(name)
     return {"message": f"{name} added successfully"}
+
+
+# DELETE USER
+@app.delete("/delete-user")
+def delete_user(name: str):
+    if name in users:
+        users.remove(name)
+        return {"message": f"{name} deleted"}
+    return {"message": "User not found"}
+
+
+# UPDATE USER
+@app.put("/update-user")
+def update_user(old_name: str, new_name: str):
+    if old_name in users:
+        index = users.index(old_name)
+        users[index] = new_name
+        return {"message": f"{old_name} updated to {new_name}"}
+    return {"message": "User not found"}
