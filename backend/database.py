83fd2f8a977d18593158a251373b928fdb28e00a
Reddyvenumadhava <yellampallevenumadhavareddy@gmail.com>
2026-04-07 11:39:00 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # 🔴 CHANGE THESE VALUES
-USERNAME = "your_username"
-PASSWORD = "your_password"
-SERVER = "candidatesql.database.windows.net"
-DATABASE = "candidate"
+DB_SERVER = os.getenv("DB_SERVER")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 DATABASE_URL = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server"
 
