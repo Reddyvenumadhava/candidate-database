a5a951b8e54bb88a744a189048a255c7ff367429
Reddyvenumadhava <yellampallevenumadhavareddy@gmail.com>
2026-04-07 07:04:04 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+# 🔴 CHANGE THESE VALUES
+USERNAME = "your_username"
+PASSWORD = "your_password"
+SERVER = "candidatesql.database.windows.net"
+DATABASE = "candidate"
+
+DATABASE_URL = f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
